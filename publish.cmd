48beb82513018613213ffabddbc4f55ff8267152
Demetrios Kalligerakis <demkal@microsoft.com>
2018-05-10 11:40:58 -0700

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=demkalssharepointbot-Web-Deploy.pubxml -p:Password=yFe3GpNfM3r3bpMi7KGpv2T2ZtjeHqKGdYldjbRtdrhqdjFgmaGKqDSxw1fG
+
